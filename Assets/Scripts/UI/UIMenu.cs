cc2df3268bf217af25d3b38f09bd31cfa536bca8
Niclas <niclas.rogulski@gmail.com>
2025-07-14 23:47:21 +1000

    basic ui menu script

---
+++ Assets/Scripts/UI/UIMenu.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public abstract class UIMenu : MonoBehaviour
+    {
+        public UIMenu parentMenu;
+        public List<UIMenu> childMenus;
+
+        public CanvasGroup canvasGroup;
+        public Button backButton;
+
+        public event Action OnOpen;
+        public event Action OnClose;
+
+        private void Awake()
+        {
+            backButton?.onClick.AddListener(Close);
+        }
+
+        private void OnEnable()
+        {
+            OnOpen += OpenTransition;
+            OnClose += CloseTransition;
+        }
+
+        private void OnDisable()
+        {
+            OnOpen -= OpenTransition;
+            OnClose -= CloseTransition;
+        }
+
+        public void Open() => OnOpen?.Invoke();
+        public void Close() => OnClose?.Invoke();
+
+        protected virtual void OpenTransition()
+        {
+            canvasGroup.blocksRaycasts = true;
+            canvasGroup.interactable = true;
+            canvasGroup.DOFade(1, 0.4f).SetEase(Ease.OutCubic);
+        }
+
+        protected virtual void CloseTransition()
+        {
+            canvasGroup.blocksRaycasts = false;
+            canvasGroup.interactable = false;
+            canvasGroup.DOFade(0, 0.4f).SetEase(Ease.OutCubic);
+        }
+    }
+}
